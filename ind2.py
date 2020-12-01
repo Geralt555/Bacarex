6aa38e889c4c29f04c57b041e6a783d96eaba9cb
Geralt555 <longer2@yandex.ru>
2020-12-01 11:26:50 +0300

    ind3.py

--- ind2.py
+++ ind2.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #  Дано предложение. Определить порядковые номера первой пары одинаковых соседних
 #  символов. Если таких символов нет, то должно быть напечатано соответствующее
 #  сообщение.
 
-line = "Змееед любит растения "
-ti, tv = 0, line[0]
-for ind, val in enumerate(line):
-    if val == tv and ind - ti == 1:
-        print(val, ' -> ', ti + 1, ind + 1)
-        break
-    ti, tv = ind, val
+if __name__ == '__main__':
+    line = "Змееед любит растения "
+    ti, tv = 0, line[0]
+    for ind, val in enumerate(line):
+        if val == tv and ind - ti == 1:
+            print(val, ' -> ', ti + 1, ind + 1)
+            break
+        ti, tv = ind, val
